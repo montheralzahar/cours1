c7885cb8faa704cd903dbb0ee7d321a2ae659f83
monther alzahar <mmonther020@gmail.com>
2024-10-05 12:54:31 +0300

    new routes

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'home page';
 });
+Route::get('/about', function () {
+    return 'about us';
+});
 Route::get('/contact', function () {
     return 'contact page';
+});
+Route::get('/services', function () {
+    return 'our services';
+});
+Route::get('/team', function () {
+    return 'our team';
+});
+Route::get('/blog', function () {
+    return 'blog page';
 });
 
 
